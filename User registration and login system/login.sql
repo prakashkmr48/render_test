f783285a242e562afa143cc07713794a11a950c0
prakashkmr48 <prakashkmr48@gmail.com>
2025-01-25 16:15:11 +0530

    Delete User registration and login system directory

--- User registration and login system/login.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE users (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(100),
-    email VARCHAR(100) UNIQUE,
-    password VARCHAR(255),
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
